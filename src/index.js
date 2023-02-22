7a3deba763acaf97fde0c27c02dd2ad70643f76a
abissaliyeva <asima.bissaliyeva@gmail.com>
2023-02-22 14:49:31 +0600

    step 8 Array as components

--- src/index.js
+++ src/index.js
@@ -6,11 +6,19 @@ import TodoList from "./todo-list";
 
 
 const App = () => {
+
+    const todoData = [
+        {label:'Drink Coffee',important:false},
+        {label:'Implement React Application', important:true},
+        {label: 'Make notes from your stud', important:false},
+        {label: 'Do not drink alcohol', important: false},
+    ];
+
     return (
         <div>
             <AppHeader/>
             <SearchPanel/>
-            <TodoList/>
+            <TodoList todos = {todoData}/>
         </div>
     )
 }
