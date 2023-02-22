2369063178d1e011857c40ccb724fae0a75ced45
abissaliyeva <asima.bissaliyeva@gmail.com>
2023-02-22 15:05:53 +0600

    step 11 Rest parameter for array elements

--- src/todo-list.js
+++ src/todo-list.js
@@ -3,8 +3,9 @@ import TodoListItem from "./todo-list-item";
 
 const TodoList = ({todos}) =>{
     const elements = todos.map((item)=>{
-        return (<li>
-            <TodoListItem {...item}/>
+        const {id , ...itemProps} = item;
+        return (<li key = {item.id}>
+                <TodoListItem{...itemProps}/>
         </li>
         );
     });
