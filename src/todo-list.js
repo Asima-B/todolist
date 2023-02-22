0910b349ae57a7925c67b19be0f7eee005a40d6f
abissaliyeva <asima.bissaliyeva@gmail.com>
2023-02-22 14:58:22 +0600

    step 10 Spread operator

--- src/todo-list.js
+++ src/todo-list.js
@@ -4,9 +4,7 @@ import TodoListItem from "./todo-list-item";
 const TodoList = ({todos}) =>{
     const elements = todos.map((item)=>{
         return (<li>
-            <TodoListItem
-                label = {item.label}
-                important = {item.important}/>
+            <TodoListItem {...item}/>
         </li>
         );
     });
